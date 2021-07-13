4ef9d46af45610d3a291e4745ec962473114b1ee
Tomás Maraschio <tomas.maraschio@gmail.com>
2021-07-13 14:46:20 -0300

    fix typo

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ class Snake:
 
 
 def main():
-    snake = Snake(15, 10)   # This mean the game field is 15x10
+    snake = Snake(15, 10)   # This means the game field is 15x10
     update_time = .125      # This is how much time there is between updates, 1/update_time = fps
     keep_playing = True
     kb = KBHit()
